82900165b75415f9530304d75647a9726597756d
ChaimaSahli <st.chaima25@gmail.com>
2025-12-18 15:09:02 +0000

    finished challenge

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: false })); 
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -14,9 +14,47 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+
+let urlDatabase = [];
+let counter = 1;
+
+
+app.post('/api/shorturl', (req, res) => {
+  const url = req.body.url; 
+  
+ 
+  if (!url || !url.startsWith('http://') && !url.startsWith('https://')) {
+    return res.json({ error: 'invalid url' });
+  }
+  
+  //! Check if it already exists
+  const existUrl = urlDatabase.find(item => item.original_url === url);
+  if (existUrl) {
+    return res.json(existUrl);
+  }
+  
+  //! Create new URL
+  const newUrl = {
+    original_url: url,
+    short_url: counter
+  };
+  
+  urlDatabase.push(newUrl);
+  counter++;
+  
+  res.json(newUrl);
+});
+
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const shortId = parseInt(req.params.short_url);
+  
+  const found = urlDatabase.find(item => item.short_url === shortId);
+  if (!found) {
+    return res.json({ error: 'No short URL found for the given input' });
+  }
+  
+  res.redirect(found.original_url);
 });
 
 app.listen(port, function() {
